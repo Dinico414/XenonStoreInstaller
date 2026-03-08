3fa4cb00b3d4e3c785912e6643b6772b2113dadf
Xenon-Ware <116182930+Dinico414@users.noreply.github.com>
2026-03-09 00:35:27 +0100

    update

--- app/src/main/java/com/xenonware/store/MainActivity.kt
+++ app/src/main/java/com/xenonware/store/MainActivity.kt
@@ -7,6 +7,7 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UseKtx")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         appListModel = ViewModelProvider(this)[AppListViewModel::class.java]
 
